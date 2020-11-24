9a944324494a4ab835646bc294aaa4d3b79fa4ef
Fabio Gomes Sakiyama <fabio.sakiyama.ecore@agcocorp.com>
2020-11-24 00:17:08 -0300

    Adicionando security que autentica em outro servidor (Keycloak)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO USUARIO (login, password, instante_Do_Cadastro) VALUES ('bla@gmail.com', '$2a$10$ep.eU6jTk6etNiew8nBoVebP/WUEGTj/7scUMUxAozGhV5cEi3yju', '2020-11-22 17:49:04.092199')
+
+INSERT INTO CATEGORIA (nome) VALUES ('Tecnologia')
+
